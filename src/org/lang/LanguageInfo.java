7e63389cdc67fbda2db7e2e3153d743667ff5beb
musthaqgit <musthaqce96@gmail.com>
2022-06-17 09:21:46 +0530

    first commit

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,30 @@
+package org.lang;
+
+public class LanguageInfo extends StateDetails {
+private void tamilLanguage() {
+System.out.println("55");
+}
+	private void englishLanguage() {
+System.out.println("55");
+	}
+private void hindiLanguage() {
+System.out.println("55");
+}
+public static void main(String[] args) {
+	LanguageInfo f = new LanguageInfo();
+	f.tamilLanguage();
+	f.englishLanguage();
+	f.hindiLanguage();
+	f.northIndia();
+	f.southIndia();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class StateDetails {
+public void southIndia() {
+System.out.println("tamil");
+}
+public void northIndia() {
+System.out.println("hindi");
+}	
+public static void main(String[] args) {
+	StateDetails g = new StateDetails();
+	g.northIndia();
+	g.southIndia();
+}	
+	
+	
+	
+	
+	
+	
+}
